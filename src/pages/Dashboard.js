de73a85df2397ab40aa795e712a272a57b7124e6
Rishabh Sawhney <rishabhsawhney@hotmail.com>
2022-04-15 13:04:03 +0200

    Created the basic boilerplate for srumify

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ @author Rishabh Sawhney
+* @function Dashboard Landing Page for the application
+*/
+
+const Dashboard = () => {
+  return(
+    <div>Dashboard</div>
+   )
+
+ }
+
+export default Dashboard
---
+++ src/pages/TicketPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+* @author Rishabh Sawhney
+* @function Pages for creating tickets
+**/
+
+const TicketPage = () => {
+  return(
+    <div>Ticket Page</div>
+   )
+
+ }
+export default TicketPage 
